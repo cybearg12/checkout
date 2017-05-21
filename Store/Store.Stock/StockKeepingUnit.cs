4b345880859508bc8697ee41d072f2463328fc02
cybearg12 <lucia.b.mormocea@gmail.com>
2017-05-21 17:11:22 +0100

    Setting up assemblies

---
+++ Store/Store.Stock/StockKeepingUnit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.Stock
+{
+    public class StockKeepingUnit
+    {
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+
+        public StockKeepingUnit(string name, decimal price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
